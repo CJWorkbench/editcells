5898a421f908e251397f3b06aa507624d0a23863
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-18 10:50:26 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="editcells",
+    version="0.0.1",
+    description="Overwrite table values one at a time",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/editcells",
+    packages=[""],
+    py_modules=["editcells"],
+    install_requires=["pandas==0.25.0"],
+)
